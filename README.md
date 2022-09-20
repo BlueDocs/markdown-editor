eb0fe169a68a775958078572c41c3bbcac21ff24
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2022-09-20 21:18:46 +0800

    chore: 完成基础开发环境 (#1)

---
+++ README.md
@@ -0,0 +1,20 @@
+# Markdown Editor
+
+Markdown 文档编辑器
+
+## 安装
+
+```bash
+# npm
+>$ npm install @bluedocs/markdown-editor --save
+
+# yarn
+>$ yarn add @bluedocs/markdown-editor
+
+# pnpm
+>$ pnpm add @bluedocs/markdown-editor
+```
+
+## 许可证
+
+[MIT](./LICENSE)
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import jsx from '@vitejs/plugin-vue-jsx';
+import dts from 'vite-plugin-dts';
+import { main, module } from './package.json';
+
+export default defineConfig({
+    plugins: [vue(), jsx(), dts()],
+    build: {
+        // 不进行语法转译，跟随项目中 webpack / vite 来进行语法转译
+        target: 'esnext',
+        lib: {
+            entry: 'src/index.ts',
+            formats: ['cjs', 'es'],
+            fileName: format => (format === 'cjs' ? main : module).replace(/^dist\//, ''),
+        },
+        rollupOptions: {},
+        minify: false,
+    },
+});
